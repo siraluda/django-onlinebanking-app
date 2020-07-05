b3b1114621397cef6dcb9ebeeb7b770b15a61258
abdul_waris <siraluda2020@gmail.com>
2020-07-05 13:55:29 -0230

    Hide secret key

--- onlinebank/onlinebank/settings.py
+++ onlinebank/onlinebank/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qvlg+&26ub56e+s&r%(*_(_r*9p2&=v(ckk=ad-daanwd&!n4='
+SECRET_KEY = os.environ.get('ONLINEBANK_APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
